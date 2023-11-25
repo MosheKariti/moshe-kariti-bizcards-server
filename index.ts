4b1d7475101d0936498082b24fee0143ec7dfc31
Moshe Kariti <moshe@tabit.cloud>
2023-11-25 16:40:55 +0200

    Remove file from tracking

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import { config } from "dotenv";
+import { connect } from "./db/connection";
+import chalk from "chalk";
+import {logger} from "./middleware/logger";
+config();
+connect();
+const app = express();
+
+app.use(express.json());
+app.use(logger);
+
+const PORT = process.env.EXPRESS_PORT;
+
+app.listen(PORT,() => {
+    console.log(chalk.blue(`App is running on http://localhost:${PORT}`));
+})
